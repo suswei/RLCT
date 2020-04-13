b8c25179de55947862d0d1a0a5ca5f4b4e66f03d
10098777 <lihui10098777@hotmail.com>
2020-04-14 00:01:27 +1000

    lr_Tanh_ReducedRank_Hypertunning 3

--- lr_Tanh_ReducedRank.sh
+++ lr_Tanh_ReducedRank.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for num in {0..215} ; do
-echo sbatch -p physical --job-name lihui10098777.${num} --account punim0890 --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((0*num)) --time=1-0:0:00 -o \"slurm_output/slurm-%A_$num.out\" --wrap=\"source activate RLCT\; module load web_proxy\; python lr_Tanh_ReducedRank.py $num\"
+echo sbatch -p physical --job-name lihui10098777.${num} --account punim0890 --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((10*num)) --time=1-0:0:00 -o \"slurm_output/slurm-%A_$num.out\" --wrap=\"source activate RLCT\; module load web_proxy\; python lr_Tanh_ReducedRank.py $num\"
 sleep 1
 
-sbatch -p physical --job-name lihui10098777.${num} --account punim0890 --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((0*num)) --time=1-0:0:00 -o "slurm_output/slurm-%A_$num.out" --wrap="source activate RLCT; module load web_proxy; python lr_Tanh_ReducedRank.py $num"
+sbatch -p physical --job-name lihui10098777.${num} --account punim0890 --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((10*num)) --time=1-0:0:00 -o "slurm_output/slurm-%A_$num.out" --wrap="source activate RLCT; module load web_proxy; python lr_Tanh_ReducedRank.py $num"
 sleep 1
 done
 echo "All jobs submitted!\n"
