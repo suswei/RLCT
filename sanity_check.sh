82118f7c9883a5b871b11504ef1752fa25bf5321
Susan Wei <stawbygl@gmail.com>
2020-04-16 10:36:43 +1000

    remove account from spartan bash

--- sanity_check.sh
+++ sanity_check.sh
@@ -10,10 +10,10 @@
 #echo "All jobs submitted!\n"
 
 for num in {0..215} ; do
-echo sbatch -p physical --job-name lihui10098777.${num} --account punim0890 --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((10*num)) --time=2-5:0:00 -o \"slurm_output/slurm-%A_$num.out\" --wrap=\"python sanity_check.py $num\"
+echo sbatch -p physical --job-name lihui10098777.${num} --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((10*num)) --time=2-5:0:00 -o \"slurm_output/slurm-%A_$num.out\" --wrap=\"python sanity_check.py $num\"
 sleep 1
 
-sbatch -p physical --job-name lihui10098777.${num} --account punim0890 --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((10*num)) --time=2-5:0:00 -o "slurm_output/slurm-%A_$num.out" --wrap="python sanity_check.py $num"
+sbatch -p physical --job-name lihui10098777.${num} --ntasks=1 --cpus-per-task=2 --mem=10000 --mail-type=END --mail-user=hui.li3@student.unimelb.edu.au --begin=now+$((10*num)) --time=2-5:0:00 -o "slurm_output/slurm-%A_$num.out" --wrap="python sanity_check.py $num"
 sleep 1
 done
 echo "All jobs submitted!\n"
