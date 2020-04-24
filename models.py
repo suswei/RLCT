code clean up, transpose fix, added variance thermodynamic asymptotics,

models.py
@@ -44,9 +44,9 @@ class LogisticRegression(nn.Module):
         x = self.linear(x)
         return F.log_softmax(x, dim=1)
 
-class Tanh(nn.Module):
+class tanh(nn.Module):
     def __init__(self, input_dim, output_dim, H):
-        super(Tanh, self).__init__()
+        super(tanh, self).__init__()
         self.fc1 = nn.Linear(input_dim, H, bias=False)
         self.fc2 = nn.Linear(H, output_dim, bias=False)
 
@@ -55,9 +55,9 @@ class Tanh(nn.Module):
         x = self.fc2(x)
         return x
 
-class ReducedRankRegression(nn.Module):
+class reducedrank(nn.Module):
     def __init__(self, input_dim, output_dim, H):
-        super(ReducedRankRegression, self).__init__()
+        super(reducedrank, self).__init__()
         self.fc1 = nn.Linear(input_dim, H, bias=False)
         self.fc2 = nn.Linear(H, output_dim, bias=False)
 