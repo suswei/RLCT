1bdc704c6cdc14cb4494f22df8435b12677acde8
Susan Wei <stawbygl@gmail.com>
2020-08-05 16:41:41 +1000

    sweep tanh

--- slurmsweep.sh
+++ slurmsweep.sh
@@ -12,7 +12,7 @@
 #SBATCH --time=0-48:0:00
 
 # Batch arrays
-#SBATCH --array=0-239
+#SBATCH --array=0-9
 
 # Send yourself an email when the job:
 # aborts abnormally (fails)
--- sweep.py
+++ sweep.py
@@ -2,41 +2,45 @@ import sys
 import os
 import itertools
 
-# def main(taskid):
-#
-#     taskid = int(taskid[0])
-#     hyperparameter_config = {
-#         'dataset': ['reducedrank_synthetic', 'tanh_synthetic'],
-#         'syntheticsamplesize': [10000, 50000],
-#         'MCs': 50*[1]
-#     }
-#     keys, values = zip(*hyperparameter_config.items())
-#     hyperparameter_experiments = [dict(zip(keys, v)) for v in itertools.product(*values)]
-#     temp = hyperparameter_experiments[taskid]
-#
-#     os.system("python3 main.py --sanity_check --dpower 0.6 "
-#               "--posterior_method implicit --epochs 200 --MCs 1 "
-#               "--betasbegin 0.1 --betasend 0.5 --betanscale --numbetas 10 "
-#               "--taskid %s --dataset %s --syntheticsamplesize %s "
-#               "--n_hidden_G 256 --num_hidden_layers_G 2 "
-#               "--n_hidden_D 8 --pretrainDepochs 2 --trainDepochs 2"
-#               %(taskid, temp['dataset'], temp['syntheticsamplesize']))
-
 def main(taskid):
 
     taskid = int(taskid[0])
     hyperparameter_config = {
-        'symmetry_factor': list(range(3, 11)),# 3 to 10
-        'num_hidden': [10, 15, 20],
-        'syntheticsamplesize': [100,250,500],
-        'mc': list(range(10))
+        'syntheticsamplesize': [100, 200],
+        'MCs': 5*[1]
     }
     keys, values = zip(*hyperparameter_config.items())
     hyperparameter_experiments = [dict(zip(keys, v)) for v in itertools.product(*values)]
     temp = hyperparameter_experiments[taskid]
 
-    os.system("python3 pyro_example.py --symmetry-factor %s --num-hidden %s --num-data %s --mc %s"
-              %(temp['symmetry_factor'],temp['num_hidden'],temp['syntheticsamplesize'],temp['mc']))
+    os.system("python3 main.py "
+              "--taskid %s "
+              "--w_dim 900 "
+              "--syntheticsamplesize %s "
+              "--dataset tanh_synthetic "
+              "--sanity_check "
+              "--betalogscale "
+              "--numbetas 5 "
+              "--MCs %s "
+              "--num-warmup 10000 "
+              "--num-samples 10000"
+              %(taskid, temp['syntheticsamplesize'], temp['MCs']))
+
+# def main(taskid):
+#
+#     taskid = int(taskid[0])
+#     hyperparameter_config = {
+#         'symmetry_factor': list(range(3, 11)),# 3 to 10
+#         'num_hidden': [10, 15, 20],
+#         'syntheticsamplesize': [100,250,500],
+#         'mc': list(range(10))
+#     }
+#     keys, values = zip(*hyperparameter_config.items())
+#     hyperparameter_experiments = [dict(zip(keys, v)) for v in itertools.product(*values)]
+#     temp = hyperparameter_experiments[taskid]
+#
+#     os.system("python3 pyro_example.py --symmetry-factor %s --num-hidden %s --num-data %s --mc %s"
+#               %(temp['symmetry_factor'],temp['num_hidden'],temp['syntheticsamplesize'],temp['mc']))
 
 
 if __name__ == "__main__":
